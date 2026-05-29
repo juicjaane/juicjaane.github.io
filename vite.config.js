d8bfce483f655e75e42e32ac2e642c8ed16f1102
Janeshvar <=>
2026-05-29 19:35:51 +0530

    Set base path for github.io root domain

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", // change to "/" if repo is named <username>.github.io
+  base: "/",
 });
